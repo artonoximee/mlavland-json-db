46ed73413c618e3de04716443814e86380521c59
Arton Oximee <artonoximee@proton.me>
2022-09-05 16:00:33 +0200

    added new json data and started layouting infos displayed

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,7 +35,18 @@ function appendData(data) {
   console.log(data);
   document.title = data.title;
   document.getElementById('logo').innerHTML = data.logo;
-  document.getElementById('description').innerText = data.description;
+  appendProjects(data.projects);
+}
+
+function appendProjects(projects) {
+  const projectsContainer = document.getElementById('projects');
+  projectsContainer.innerHTML = '';
+  for (let i = 0; i < projects.length; i++) {
+    let project = document.createElement('p');
+    project.innerHTML = `<b>${projects[i].title}</b> <br> ${projects[i].date}, ${projects[i].type}`;
+    projectsContainer.appendChild(project);
+  }
+  console.log(projects);
 }
 
 // Default language on first load
