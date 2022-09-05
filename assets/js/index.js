0b77d0be57d47be3f59fc969099d7bf5a642ed50
Arton Oximee <artonoximee@proton.me>
2022-09-05 11:23:23 +0200

    tested json data display in browser

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,35 @@
+/*
+async function fetchDatabase() {
+  fetch('../../db/general.json')
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then(function(database) {
+      return database
+    });
+}
+*/
+
 fetch('../../db/general.json')
-  .then(function(response) {
-    if (response.ok) {
-      return response.json();
-    }
-  })
-  .then(function(db) {
-    console.log(db);
-  });
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+function appendData(data) {
+  console.log(data);
+  document.getElementById('title').innerText = data.title;
+  /*  
+  var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
+        mainContainer.appendChild(div);
+    }*/
+}
